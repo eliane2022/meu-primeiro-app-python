6b0682a5947fae237432c4dbb620cc8b4ced4281
eliane2022 <elianefon2005@gmail.com>
2022-10-25 11:53:14 -0300

    adiç/cal/imc
    adiçao

--- olamundo.py
+++ olamundo.py
@@ -3,10 +3,11 @@ nome = input("digite seu nome: ")
 idade = int(input("digite sua idade: "))
 altura = float(input("digite sua altura: "))
 peso = float(input("digite seu peso: "))
-
+imc= peso/altura**2
 print("-"* 30)
 print("os dados coletados foram: ")
 print("seu nome: ",nome)
 print("idade: ",idade," anos")
 print("altura: ",altura)
 print("peso:  ",peso, " kgs")
+
