4fcb115f9463be89fffe58f937dfe2d5513cc7eb
eliane2022 <elianefon2005@gmail.com>
2022-10-25 11:19:23 -0300

    criado app imc
    inicio de desenv/algori de imc

---
+++ olamundo.py
@@ -0,0 +1,12 @@
+#coletando dados do usuario
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+altura = float(input("digite sua altura: "))
+peso = float(input("digite seu peso: "))
+
+print("-"* 30)
+print("os dados coletados foram: ")
+print("seu nome: ",nome)
+print("idade: ",idade," anos")
+print("altura: ",altura)
+print("peso:  ",peso, " kgs")
